6e2534f9c2c8779401222edaa3b441872d833539
ovi <mdovi0177688@gmail.com>
2025-04-25 03:23:30 +0800

    all file

--- ps-4.py
+++ ps-4.py
@@ -1,1 +1,10 @@
 # Python Program to Calculate the Area of a Triangle(ত্রিভুজের ক্ষেত্রফল গণনার জন্য পাইথন প্রোগ্রাম)
+# we know :ত্রিভুজের ক্ষেত্রফলের সূত্র হলো:ক্ষেত্রফল = (ভূমি * উচ্চতা) / 2
+
+base = 45
+hight = 22
+
+area = (45*22) / 2
+print(f"{area}")
+
+
--- ps-5.py
+++ ps-5.py
@@ -1,30 +1,33 @@
 # Python Program to Solve Quadratic Equation(দ্বিঘাত সমীকরণ সমাধানের জন্য পাইথন প্রোগ্রাম)
+# we know: (Quadratic Formula): x = -b ± b*2 −4ac by 2a​
 
 import cmath
 def solve_quabratic_equation(a,b,c):
-    delta = (b*2) - 4*(a*c)
+  delta = (b*2) -4*(a*c)
 
-    if delta >= 0:
-        root1 = (-b + delta**0.5) / (2*a)
-        root2 = (-b - delta**0.5) / (2*a)
-        return root1, root2
-    else:
-        root1 = (-b + cmath.sqrt(delta) / (2*a) )
-        root2 = (-b - cmath.sqrt(delta) / (2*a) )
-        return root1, root2
-    
-a = 6
-b = -8
-c = 2
+  if delta >0:
+    root1 = (-b + delta**.05) / (2*a)
+    root2 = (-b - delta**.05) / (2*a)
+    return root1,root2
 
-solutions = solve_quabratic_equation(a,b,c)
+  else:
+    root1 = (-b + cmath.sqrt(delta) / (2*a))
+    root2 = (-b + cmath.sqrt(delta) / (2*a))
+    return root1,root2
+
+a = 4
+b = -7
+c = 8
+
+solutions =  solve_quabratic_equation(a,b,c)
 
 if isinstance(solutions,tuple):
-    if solutions[0] == solutions[1]:
-        print( f"The equation has a real seed: {solutions[0]}")
-    else:
-        print( f"The two real seeds of the equation are: {solutions[0]} and {solutions[1]}")
-
+  if solutions[0] == solutions[1]:
+    print( f"Only one real solution: {solutions[0]}")
+  else:
+    print(f"Two different practical solutions: {solutions[0]} and {solutions[1]}")
 
 elif isinstance(solutions, complex):
-  print(f"The two complex roots of the equation are: {solutions[0]} এবং {solutions[1]}")
+  print(f"The two complex roots of the equation are: {solutions[0]} and {solutions[1]}")
+  
+
